0b2a21c073f613ddc1a2efa74434bc7401f1a742
EvgeniyBoykov <88290040+EvgeniyBoykov@users.noreply.github.com>
2021-09-25 23:35:55 +0400

    Home_Work_5

--- src/HomWork_5/main.java
+++ src/HomWork_5/main.java
@@ -4,13 +4,10 @@ import java.util.Arrays;
 
 public class main {
         public static void main(String[] args) {
-
             AppData appData = new AppData();
             appData.load("HomWork5.txt");
-
             System.out.println(Arrays.toString(appData.getHeader()));
             System.out.println(Arrays.deepToString(appData.getData()));
-
             appData.save("HomWork5-1.txt");
         }
     }
