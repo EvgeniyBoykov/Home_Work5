2ea99729ed02315aeace7966db4ffd236cf5be3e
EvgeniyBoykov <88290040+EvgeniyBoykov@users.noreply.github.com>
2021-09-25 23:34:45 +0400

    Initial commit

---
+++ src/HomWork_5/main.java
@@ -0,0 +1,16 @@
+package HomWork_5;
+
+import java.util.Arrays;
+
+public class main {
+        public static void main(String[] args) {
+
+            AppData appData = new AppData();
+            appData.load("HomWork5.txt");
+
+            System.out.println(Arrays.toString(appData.getHeader()));
+            System.out.println(Arrays.deepToString(appData.getData()));
+
+            appData.save("HomWork5-1.txt");
+        }
+    }
